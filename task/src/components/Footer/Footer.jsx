52fa672c1359ecfb77a056887f264361d56da297
Auwalu Muhammad Izziddin <auwalmuhammad5559@gmail.com>
2025-01-12 21:58:25 +0100

    commmit

--- task/src/components/Footer/Footer.jsx
+++ task/src/components/Footer/Footer.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa'; // Importing icons
 import './Footer.css';
 
 const Footer = () => (
   <footer className="footer">
     <p>Contact us at: auwaluizziddin2212@gmail.com</p>
     <div className="social-links">
-      <a href="#facebook">Facebook</a>
-      <a href="#twitter">Twitter</a>
-      <a href="#instagram">Instagram</a>
+      <a href="#facebook" aria-label="Facebook">
+        <FaFacebook size={24} />
+      </a>
+      <a href="#twitter" aria-label="Twitter">
+        <FaTwitter size={24} />
+      </a>
+      <a href="#instagram" aria-label="Instagram">
+        <FaInstagram size={24} />
+      </a>
     </div>
   </footer>
 );
